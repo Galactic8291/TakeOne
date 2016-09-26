e52ac233fb7f4bad3961dfdd36629d7e9155784e
Robbie McClellan <robbiemcclellan14@gmail.com>
2016-09-25 20:57:46 -0400

    remove pieces of example

--- src/components/Bar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Bar = () => (
-  <div>I Am Bar</div>
-)
-
-export default Bar
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import { App, Home, Foo, Bar } from '../components/module'
+import { App, Home } from '../components/module'
 
 const routes = (
   <Route path='/' component={App}>
     <IndexRoute component={Home} />
-    <Route path='foo' component={Foo} />
-    <Route path='bar' component={Bar} />
   </Route>
 )
 
